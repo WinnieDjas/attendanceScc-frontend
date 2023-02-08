c7696616997d25fff1c897c7321461620a40d9c4
winnyh20 <winny.ihirwe@gmail.com>
2023-02-08 00:09:54 +0000

    home commit

--- frontend/src/View/Home.js
+++ frontend/src/View/Home.js
@@ -4,10 +4,11 @@ import "./Home.css"
 function Home() {
   return (
     <div className="HomeContainer">
-      {/* <img src="https://res.cloudinary.com/dggvxdkri/image/upload/v1665065683/samples/Attendance%20pict/ph_36956_141951_lnlbyv.jpg"  alt=''/> */}
-      <img src="https://res.cloudinary.com/dggvxdkri/image/upload/v1665065699/samples/Attendance%20pict/Regtank-KYC-Compliance-Made-Simple-1024x683_ogu3gc.png"  alt=''/>
-      
+      <div className="homeModel"> 
       </div>
+      </div>
+      
+
   )
 }
 
