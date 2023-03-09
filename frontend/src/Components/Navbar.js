2957d2def226499b70b8879622a4ef6ad3a30f76
winnyh20 <winny.ihirwe@gmail.com>
2023-03-09 01:01:56 +0000

    Navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,10 +1,26 @@
-import React from 'react'
-// import './navbar.css'
+// import React, { Component } from 'react'
+import './navbar.css'
+import { MenuItems } from './MenuItems'
 import { Link } from 'react-router-dom'
+// import HomeIcon from '@mui/icons-material/Home';
 
 const Navbar =()=> {
   return (
-    <></>
+    <nav className='NavbarItems'>
+      <h1 className='Nav-Logo'>Attendance</h1>
+      <ul className='Nav-menu'>
+        {MenuItems.map((Item, Index) =>{
+          return (
+            <li key={Index}>
+            <a href='/'>
+              {/* <i className="HomeIcon"></i> */}
+              {Item.title}</a>
+          </li>
+          )
+        })}
+        
+      </ul>
+    </nav>
   )
 }
 
